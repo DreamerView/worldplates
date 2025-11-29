053be6f44a25e4b1886a6c8456bdcc0c10b9a604
dreamerview <temirkhan.onyx@gmail.com>
2025-11-29 11:41:03 +0500

    changed categories

--- src/components/Categories/Loader.vue
+++ src/components/Categories/Loader.vue
@@ -1,10 +1,9 @@
 <template>
-    <div class="d-flex gap-2 overflow-x-hidden pb-2">
-        <div class="bg-body-secondary border rounded-5 p-3 d-flex flex-column gap-2 w-100" v-for="item in arr">
-            <div class="bg-body rounded-4 mx-auto" style="width:80px;height:50px">
-
+    <div class="d-flex gap-3 overflow-x-hidden pb-2">
+        <div class="d-flex flex-column gap-2" v-for="item in arr">
+            <div class="bg-body-secondary border rounded-5 d-flex align-items-center justify-content-center" style="width:80px;height:80px;">
             </div>
-            <h6 class="text-center m-0">Loading</h6>
+            <p class="text-center text-truncate m-0 fw-bold" style="font-size:0.8rem;">Loading</p>
         </div>
     </div>
 </template>
--- src/components/Categories/Main.vue
+++ src/components/Categories/Main.vue
@@ -7,10 +7,12 @@
             {{errorText }}
         </div>
         <div v-else>
-            <div class="d-flex gap-2 overflow-x-hidden pb-2">
-                <div class="bg-body-secondary border rounded-5 p-3 d-flex flex-column gap-2 w-100" v-for="item in (data?.categories || [])" :key="item.idCategory">
-                    <img v-if="item.strCategoryThumb" class="rounded-4 mx-auto" loading="lazy" :src="item.strCategoryThumb" width="auto" height="48" alt=""/>
-                    <p class="text-center text-truncate m-0">{{ item?.strCategory || "Unknown" }}</p>
+            <div class="d-flex gap-3 overflow-x-hidden pb-2">
+                <div class="d-flex flex-column gap-2" v-for="item in (data?.categories || [])" :key="item.idCategory">
+                    <div class="bg-body-secondary border rounded-5 d-flex align-items-center justify-content-center" style="width:80px;height:80px;">
+                        <img v-if="item.strCategoryThumb" class="rounded-4 mx-auto" loading="lazy" :src="item.strCategoryThumb" width="auto" height="40" alt=""/>
+                    </div>
+                    <p class="text-center text-truncate m-0 fw-bold" style="font-size:0.8rem;">{{ item?.strCategory || "Unknown" }}</p>
                 </div>
             </div>
         </div>
