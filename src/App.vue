5590e39716914c8d386a60b96c6904b340340f91
dreamerview <temirkhan.onyx@gmail.com>
2025-11-29 11:32:33 +0500

    added logo

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,11 @@
 <template>
+  <header class="container-xl my-4">
+    <div class="row">
+      <div class="col-6">
+        <img style="max-width:200px;width:100%;height:auto;aspect-ratio: 41/12;" loading="lazy" src="/logo.png" alt=""/>
+      </div>
+      <div class="col-6"></div>
+    </div>
+  </header>
   <router-view />
 </template>
