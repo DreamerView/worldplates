5cc5e004d0aca592e8047c68a47d03559c5c659f
dreamerview <temirkhan.onyx@gmail.com>
2025-11-30 04:22:13 +0500

    added ingredients

--- src/views/View.vue
+++ src/views/View.vue
@@ -5,16 +5,37 @@
                 <div v-if="loading" class="w-100 h-auto bg-body-secondary rounded-5" style="aspect-ratio: 1;"/>
                 <img v-else :src="data?.meals[0]?.strMealThumb" class="w-100 h-auto bg-body-secondary rounded-5" style="aspect-ratio: 1;" alt=""/>
             </div>
-            <div class="col-lg-6 col-md-4 col-sm-6 d-flex flex-column align-items-sm-center justify-content-center gap-4">
-                <h1 class="m-0">{{ data?.meals[0]?.strMeal || "Unknown" }}</h1>
-                <p class="m-0">Area: {{ data?.meals[0]?.strArea || "Unknown" }}</p>
-                <p class="m-0">Category: {{ data?.meals[0]?.strCategory || "Unknown" }}</p>
-                {{ data?.meals[0]?.strTags }}
-            </div>
-            <div class="col-lg-3 col-md-4">
-                <div class="bg-body-secondary border p-4 h-100 rounded-5">
-                    <h3>Ingredients</h3>
+            <div class="col-lg-6 col-md-4 col-sm-6">
+              <div class="d-flex flex-column justify-content-center  gap-1 h-100 ms-md-5">
+                <span class="text-muted">
+                  {{ data?.meals[0]?.strCategory || "Unknown" }}
+                </span>
+                <h1 class="m-0 mt-2">{{ data?.meals[0]?.strMeal || "Unknown" }}</h1>
+                <div class="mt-4">
+                  <p class="m-0 mb-2">Area: {{ data?.meals[0]?.strArea || "Unknown" }}</p>
+                  <p class="m-0">
+                    Source:
+                    <a
+                      v-if="data?.meals[0]?.strSource"
+                      :href="data.meals[0].strSource"
+                      target="_blank"
+                      rel="noopener noreferrer"
+                    >
+                      Click here
+                    </a>
+                    <span v-else>Unknown</span>
+                  </p>
                 </div>
+                {{ data?.meals[0]?.strTags }}
+              </div>
+            </div>
+            <div class="col-lg-3 col-md-3">
+              <div class="d-flex flex-column gap-3 h-100 overflow-y-auto">
+                <div class="bg-body-secondary border py-1 px-3 rounded-5"
+                    v-for="item in getIngredients(data?.meals[0])">
+                  {{ item.name }}: <b>{{ item.value }}</b>
+                </div>
+              </div>
             </div>
             <div class="col-lg-6">
                 <div class="bg-body-secondary p-4 rounded-4">
@@ -37,12 +58,24 @@ const props = defineProps({
   id: String
 });
 
-function getYouTubeId(url) {
-  if (!url || typeof url !== "string") return null;
-  const regExp =
-    /^.*(youtu.be\/|v\/|u\/\w\/|embed\/|watch\?v=|watch\?.+&v=)([^#&?]*).*/;
-  const match = url.match(regExp);
-  return (match && match[2].length === 11) ? match[2] : null;
+function getIngredients(meal) {
+  const result = [];
+
+  if(!meal) return result;
+
+  for (let i = 1; i <= 20; i++) {
+    const name = meal[`strIngredient${i}`]?.trim();
+    const value = meal[`strMeasure${i}`]?.trim();
+    console.log(name)
+    if (name && name !== "") {
+      result.push({
+        name,
+        value: value || ""
+      });
+    }
+  }
+
+  return result;
 }
 
 const { data, loading, error, errorText, execute } = useFetch(
