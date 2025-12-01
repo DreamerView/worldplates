5527685a9fb7d34814804b92b8369a712dad1c77
dreamerview <temirkhan.onyx@gmail.com>
2025-12-01 14:59:49 +0500

    changed policy load

--- src/components/CategoriesItem/Block.vue
+++ src/components/CategoriesItem/Block.vue
@@ -2,7 +2,7 @@
     <div class="row g-4">
         <router-link :to="'/view/'+item.idMeal" style="text-decoration: none;" v-for="item in list" class="col-xl-2 col-lg-3 col-md-4 col-6">
             <div class="d-flex flex-column gap-2">
-                <SmartImg :src="item.strMealThumb + '/small'" :srcset="`${item.strMealThumb}/medium 1x, ${item.strMealThumb}/large 2x`"  class="rounded-5 w-100 h-auto" style="aspect-ratio: 1;" />
+                <SmartImg :src="item.strMealThumb + '/small'"  class="rounded-5 w-100 h-auto" style="aspect-ratio: 1;" />
                 <h6 class="text-truncate mt-2 text-center m-0 text-body" style="width:calc(100% - 16px)">
                     {{ item?.strMeal || "Unknown" }}</h6>
             </div>
--- src/components/CountryItem/Block.vue
+++ src/components/CountryItem/Block.vue
@@ -2,10 +2,7 @@
     <div class="row g-4">
         <router-link :to="'/view/'+item.idMeal" style="text-decoration: none;" v-for="item in list" class="col-xl-2 col-lg-3 col-md-4 col-6">
             <div class="d-flex flex-column gap-2">
-                <img :src="item.strMealThumb + '/small'" :srcset="`
-                                ${item.strMealThumb}/medium 1x,
-                                ${item.strMealThumb}/large 2x
-                            `" class="rounded-5 w-100 h-auto bg-body-secondary" style="aspect-ratio: 1;" alt="" loading="lazy" />
+                <img :src="item.strMealThumb + '/small'" class="rounded-5 w-100 h-auto bg-body-secondary" style="aspect-ratio: 1;" alt="" loading="lazy" />
                 <h6 class="text-truncate mt-2 text-center m-0 text-body" style="width:calc(100% - 16px)">
                     {{ item?.strMeal || "Unknown" }}</h6>
             </div>
